1d74ed41ab8968068ad0370b2e5740f72c3d055f
Manuel <manuel.chouhy@correo.ucu.edu.uy>
2024-08-07 10:32:07 -0300

    Esta clase implementa la operación <nombre-operación

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -2,6 +2,7 @@
 Console.WriteLine(Substraction.Substract(10, 1));
 Console.WriteLine(Multiplication.Multiply(3, 3));
 Console.WriteLine(Division.Divide(18, 2));
+// Esta clase implementa la operación <nombre-operación>
 public class Multiplication
 {
     public static int Multiply(int a, int b)
