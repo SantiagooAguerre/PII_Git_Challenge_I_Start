ea94c2e48f35aeea588491d4e044afe07fa9c4e6
Facundo-Farnes <facundo.farnes@correo.ucu.edu.uy>
2024-08-07 10:29:56 -0300

    Comentario agregado

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -3,6 +3,8 @@ Console.WriteLine(Substraction.Substract(10, 1));
 Console.WriteLine(Multiplication.Multiply(3, 3));
 Console.WriteLine(Division.Divide(18, 2));
 
+// Esta clase implementa la operación <Division>
+
  git public class Division
 {
     public static double Divide(int a, int b)
